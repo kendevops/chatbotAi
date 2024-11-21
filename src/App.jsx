9f708673f196dffa3660df3f7ce2769727a138c9
Kenneth Aladi <kendevops@gmail.com>
2024-11-21 09:20:04 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center text-4xl text-black">
+        Hands on training on Chatbot building with AWS Amplify AI Kit
+      </div>
+    </>
+  );
+}
+
+export default App;
